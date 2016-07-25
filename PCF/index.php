02db6ac44f9fcfa53a7f9b9d3c44549c9405ff5a
Amadeusz Opac <amadeuszopac@gmail.com>
2016-07-25 10:18:18 +0200

    css coloring part 1

--- PCF/index.php
+++ PCF/index.php
@@ -7,12 +7,12 @@
  */
 echo '<!DOCTYPE html>
 <html><head>
-<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="../style.css">
 
 </head>';
 
 echo '
-<script src="jquery-3.1.0.min.js"></script>
+<script src="../jquery-3.1.0.min.js"></script>
 <body>
 <div align="center"><table id="table">
         <tr>
@@ -29,46 +29,46 @@ echo '
         <tr>
             <td rowspan="2" id="LVs 1-2">3x</td>
             <td id="Lv.1">6x</td>
-            <td class="clickable">Yellow<br>Lv.1<br>24x</td>
-            <td class="clickable">Pink<br>  Lv.1<br>24x</td>
-            <td class="clickable">Blue<br>  Lv.1<br>24x</td>
-            <td class="clickable">Green<br> Lv.1<br>24x</td>
+            <td class="clickable yellow">Yellow<br>Lv.1<br>24x</td>
+            <td class="clickable pink">  Pink<br>  Lv.1<br>24x</td>
+            <td class="clickable blue">  Blue<br>  Lv.1<br>24x</td>
+            <td class="clickable green"> Green<br> Lv.1<br>24x</td>
         </tr>
         <tr>
             <td id="Lv.2">6x</td>
-            <td class="clickable">Yellow<br>Lv.2<br>24x</td>
-            <td class="clickable">Pink<br>  Lv.2<br>24x</td>
-            <td class="clickable">Blue<br>  Lv.2<br>24x</td>
-            <td class="clickable">Green<br> Lv.2<br>24x</td>
+            <td class="clickable yellow">">Yellow<br>Lv.2<br>24x</td>
+            <td class="clickable pink">  ">Pink<br>  Lv.2<br>24x</td>
+            <td class="clickable blue">  ">Blue<br>  Lv.2<br>24x</td>
+            <td class="clickable green"> ">Green<br> Lv.2<br>24x</td>
         </tr>
          <tr>
             <td rowspan="2" id="LVs 3-4">3x</td>
             <td id="Lv.3">6x</td>
-            <td class="clickable">Yellow<br>Lv.3<br>24x</td>
-            <td class="clickable">Pink<br>  Lv.3<br>24x</td>
-            <td class="clickable">Blue<br>  Lv.3<br>24x</td>
-            <td class="clickable">Green<br> Lv.3<br>24x</td>
+            <td class="clickable yellow">">Yellow<br>Lv.3<br>24x</td>
+            <td class="clickable pink">  ">Pink<br>  Lv.3<br>24x</td>
+            <td class="clickable blue">  ">Blue<br>  Lv.3<br>24x</td>
+            <td class="clickable green"> ">Green<br> Lv.3<br>24x</td>
         </tr>
         <tr>
             <td id="Lv.4">6x</td>
-            <td class="clickable">Yellow<br>Lv.4<br>24x</td>
-            <td class="clickable">Pink<br>  Lv.4<br>24x</td>
-            <td class="clickable">Blue<br>  Lv.4<br>24x</td>
-            <td class="clickable">Green<br> Lv.4<br>24x</td>
+            <td class="clickable yellow">">Yellow<br>Lv.4<br>24x</td>
+            <td class="clickable pink">  ">Pink<br>  Lv.4<br>24x</td>
+            <td class="clickable blue">  ">Blue<br>  Lv.4<br>24x</td>
+            <td class="clickable green"> ">Green<br> Lv.4<br>24x</td>
         </tr> <tr>
             <td rowspan="2" id="LVs 5-6">3x</td>
             <td id="Lv.5">6x</td>
-            <td class="clickable">Yellow<br>Lv.5<br>24x</td>
-            <td class="clickable">Pink<br>  Lv.5<br>24x</td>
-            <td class="clickable">Blue<br>  Lv.5<br>24x</td>
-            <td class="clickable">Green<br> Lv.5<br>24x</td>
+            <td class="clickable yellow">">Yellow<br>Lv.5<br>24x</td>
+            <td class="clickable pink">  ">Pink<br>  Lv.5<br>24x</td>
+            <td class="clickable blue">  ">Blue<br>  Lv.5<br>24x</td>
+            <td class="clickable green"> ">Green<br> Lv.5<br>24x</td>
         </tr>
         <tr>
             <td id="Lv.6">6x</td>
-            <td class="clickable">Yellow<br>Lv.6<br>24x</td>
-            <td class="clickable">Pink  <br>Lv.6<br>24x</td>
-            <td class="clickable">Blue  <br>Lv.6<br>24x</td>
-            <td class="clickable">Green <br>Lv.6<br>24x</td>
+            <td class="clickable yellow">">Yellow<br>Lv.6<br>24x</td>
+            <td class="clickable pink">  ">Pink  <br>Lv.6<br>24x</td>
+            <td class="clickable blue">  ">Blue  <br>Lv.6<br>24x</td>
+            <td class="clickable green"> ">Green <br>Lv.6<br>24x</td>
         </tr>
 </table>';
 
