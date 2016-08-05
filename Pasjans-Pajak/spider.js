38b45a08f98405becfbb9901d9258cba51d80b3b
Amadeusz Opac <amadeuszopac@gmail.com>
2016-08-05 13:18:17 +0200

    starting dialog and header

--- Pasjans-Pajak/spider.js
+++ Pasjans-Pajak/spider.js
@@ -2,7 +2,77 @@
  * Created by amade on 03.08.2016.
  */
 
+var score = 0;
+var gameTime;
+var rectList;
 
-$( "#dialog" ).dialog().dialog( "option", "width", 800);
-$( "#dialog-button" ).button();
+
+$( "#dialog" ).dialog({
+    width: 800,
+    buttons: [
+        {
+            text: "Graj!",
+            click: function () {
+                populateBoard(getDifficultyLevel());
+                $(this).dialog("close");
+            }
+        }]
+});
 $( "#radioset" ).buttonset();
+$( "div.button" ).button();
+
+
+function getDifficultyLevel(){
+    if ($("#radio1").attr("checked")=== "checked") return 1;
+    if ($("#radio2").attr("checked")=== "checked") return 2;
+    if ($("#radio3").attr("checked")=== "checked") return 3;
+}
+
+function populateBoard(diffLevel)
+{
+    var diffText = "";
+    switch(diffLevel){
+        case 1:
+            diffText = "Łatwy (Jeden kolor)";
+            break;
+        case 2:
+            diffText = "Średni (Dwa kolory)";
+            break;
+        case 3:
+            diffText = "Trudny (Cztery kolory)";
+            break;
+    }
+
+    drawCards();
+
+
+
+
+    $("#difficulty").html(diffText);
+    $("#score").html(score);
+    $("#wrapper").removeAttr("style");
+
+
+
+    gameTime = 0;
+    setInterval(function(){
+        gameTime++;
+        $("#gameTime").html(stringToDate(gameTime));
+    }, 100)
+}
+
+function stringToDate(tenthsOfSecond){
+var returnString = "";
+    returnString += Math.floor(tenthsOfSecond/600);
+    returnString += ":" + Math.floor((tenthsOfSecond%600)/10);
+    returnString += "," + (tenthsOfSecond%10) + "s";
+    return returnString;
+}
+
+function newGame(){
+    location.reload();
+}
+
+function drawCards(){
+    console.log("DrawCards");
+}
