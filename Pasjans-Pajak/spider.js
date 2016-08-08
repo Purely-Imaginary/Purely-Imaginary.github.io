6a2882a6ec05a3afad621aac70cacc66745b2fcd
Amadeusz Opac <amadeuszopac@gmail.com>
2016-08-08 13:25:34 +0200

    framelogic initial

--- Pasjans-Pajak/spider.js
+++ Pasjans-Pajak/spider.js
@@ -4,7 +4,7 @@
 
 var score = 0;
 var gameTime;
-var rectList;
+var rectList = [];
 
 
 $( "#dialog" ).dialog({
@@ -43,7 +43,7 @@ function populateBoard(diffLevel)
             break;
     }
 
-    drawCards();
+
 
 
 
@@ -51,7 +51,7 @@ function populateBoard(diffLevel)
     $("#difficulty").html(diffText);
     $("#score").html(score);
     $("#wrapper").removeAttr("style");
-
+    drawCards();
 
 
     gameTime = 0;
@@ -74,5 +74,72 @@ function newGame(){
 }
 
 function drawCards(){
+    createCards(2);
+    var canvas = document.getElementById("playCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.canvas.width  = window.innerWidth-100;
+    ctx.canvas.height = window.innerHeight-100;
+    rectList = shuffle(rectList);
+    for (var i=0;i<rectList.length;i++)
+    {
+        var x = (i%10)*120;
+        var y = Math.floor(i/10)*40;
+        drawCardObject(ctx,rectList[i],x,y);
+    }
+
     console.log("DrawCards");
+}
+
+function drawCardObject(canvas,card,x,y)
+{
+    card.x = x;
+    card.y = y;
+    if (card.color === 0) canvas.fillStyle = "#000000";
+    if (card.color === 1) canvas.fillStyle = "#FF0000";
+
+    canvas.fillRect(x,y,100,180);
+
+    canvas.strokeStyle = 'white';
+    canvas.strokeRect(x,y,100,180);
+}
+function createCards(difficultyLvl) {
+    rectList = [];
+    if (difficultyLvl === 2) {
+        for (var i = 0; i < 4; i++) {
+            for (var j = 0; j < 2; j++) {
+                for (var k = 1; k < 14; k++) {
+                    var card = {
+                        color: j,
+                        value: k,
+                        isFlipped: 0,
+                        x: 0,
+                        y: 0
+                    }
+                    rectList.push(card);
+                }
+            }
+
+        }
+        console.log(rectList.length + " cards created for medium difficulty");
+    }
+    console.log(rectList);
+}
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
 }
---
+++ framelogic.js
@@ -0,0 +1,103 @@
+javascript:(function(){
+
+
+$("td.satellites").each(function(){
+$(this).css({'color': 'black', 'background-color': 'lightgreen'} );
+if ($(this).html()<5) $(this).css({'color': 'black', 'background-color': 'yellow'} );
+if ($(this).html()==="0") $(this).css({'color': 'black', 'background-color': 'red', 'font-weight': 'bold'} );
+});
+console.log("Przeliczanie satelit (<5)");
+
+$("td.latitude").each(function(){
+if ($(this).html().indexOf("0,0000")!==-1) {
+$(this).css({'color': 'black', 'background-color': 'red', 'font-weight': 'bold'});
+$(this).html("0,0000");
+}
+else $(this).css({'color': 'black', 'background-color': 'lightgreen'} );
+});
+
+
+$("td.longitude").each(function(){
+if ($(this).html().indexOf("0,0000")!==-1) {
+$(this).css({'color': 'black', 'background-color': 'red', 'font-weight': 'bold'} );
+$(this).html("0,0000");
+}
+else $(this).css({'color': 'black', 'background-color': 'lightgreen'} );
+});
+console.log("Przeliczanie d³ugoœci i szerokoœci (!=0,000)");
+
+$("td.memory").each(function(){
+if ($(this).html()>300) $(this).css({'color': 'black', 'background-color': 'red', 'font-weight': 'bold'} );
+else $(this).css({'color': 'black', 'background-color': 'lightgreen'} );
+});
+console.log("Przeliczanie pamiêci (>300)");
+
+$("td.network").each(function(){
+if ($(this).html()===0) $(this).css({'color': 'black', 'background-color': 'red', 'font-weight': 'bold'} );
+else $(this).css({'color': 'black', 'background-color': 'lightgreen'} );
+});
+console.log("Przeliczanie kodu sieci (!=0)");
+
+$("td.power").each(function(){
+if ($(this).html()<10) $(this).css({'color': 'black', 'background-color': 'red', 'font-weight': 'bold'} );
+else $(this).css({'color': 'black', 'background-color': 'lightgreen'} );
+});
+console.log("Przeliczanie napiêcia akumulatora (<10)");
+
+$("td.battery").each(function(){
+if ($(this).html()<8) $(this).css({'color': 'black', 'background-color': 'red', 'font-weight': 'bold'} );
+else $(this).css({'color': 'black', 'background-color': 'lightgreen'} );
+});
+console.log("Przeliczanie baterii (<8)");
+
+$("td.antenna").each(function(){
+if ($(this).html()==="1") $(this).css({'color': 'black', 'background-color': 'lightgreen'} );
+else $(this).css({'color': 'black', 'background-color': 'red', 'font-weight': 'bold'} );
+});
+console.log("Przeliczanie antstatu (!=1)");
+
+$("td.signal").each(function(){
+if ($(this).html()<16) $(this).css({'color': 'black', 'background-color': 'yellow'} );
+else if ($(this).html()==="0") $(this).css({'color': 'black', 'background-color': 'red', 'font-weight': 'bold'} );
+else $(this).css({'color': 'black', 'background-color': 'lightgreen'} );
+});
+console.log("Przeliczanie zasiêgu GSM (<16 / ==0)");
+
+$("td.frame").each(function(){
+if ($(this).html()!=="3 ") $(this).css({'color': 'black', 'background-color': 'yellow'} );
+else $(this).css({'color': 'black', 'background-color': 'lightgreen'} );
+});
+console.log("Przeliczanie statusu ramki (!=3)");
+
+$("td.inputs, td.alarm").each(function(){
+var input = $(this).html();
+var printString = "<span style='color:blue'>  (";
+printString += parseInt(input).toString(2);
+
+var dins = "\<";
+for (var i=0;i<printString.length;i++)
+{
+if (printString[i] === "1") dins += (printString.length-i)-1 + ",";
+}
+if (dins === "\<") dins += " ";
+$(this).html(input + printString + ")</span> <span style='color:green'>" + dins.slice(0, -1) + "\></span>");
+});
+console.log("Przeliczanie dinów i alarmu");
+
+$(".validity, .altitude, .course").each(function(){
+$(this).css('display','none');
+});
+console.log("Chowanie 'Praw.odczyt','Wysokoœæ','Kierunek'");
+
+$("th.frame").html("Typ");
+$("th.memory").html("Pamiêæ");
+$("th.signal").html("GSM");
+$("th.antenna").html("Antena");
+
+$("th.alarm").html("<a href='http://frametech.framelogic.pl/index.php/Znaczenie_poszczeg%C3%B3lnych_bit%C3%B3w_pola_-_alarm'>Alarm</a>");
+
+console.log("Zmiana nag³ówków Typ,Pamiêæ,GSM,Antena i Alarm");
+
+
+console.log("Pomocnik odbioru v1.2.2");
+})();
