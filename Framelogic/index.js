ddc267a86fdc5188808776230da8dbc4a0b8d7e9
Amadeusz Opac <amadeuszopac@gmail.com>
2016-08-09 22:14:20 +0200

    index tinker

--- Framelogic/index.js
+++ Framelogic/index.js
@@ -2,5 +2,5 @@
  * Created by amade on 09.08.2016.
  */
 function load_website_into_id(website,id){
-    document.getElementById(id).innerHTML='<object type="text/html" data='+website+' ></object>';
+    document.getElementById(id).innerHTML='<object type="text/html" data='+website+' style="height: 100%; width: 100%"></object>';
 }
