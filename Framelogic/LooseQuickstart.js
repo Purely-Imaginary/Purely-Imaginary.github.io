84dccf5e8149d96924092e76dcce2bd4483056ac
Amadeusz Opac <amadeuszopac@gmail.com>
2016-08-09 06:58:37 +0200

    Loose quickstart

---
+++ Framelogic/LooseQuickstart.js
@@ -0,0 +1,18 @@
+javascript:(function () {
+
+    var url = window.location.href;
+    var flota = url.split(".")[0].split("//")[1];
+
+    if (url.indexOf("loose") !== -1) {
+        $(".action_raports_showcard_showvehicle_pojazd_id").each(function () {
+            var id = $(this).attr('value');
+            $(this).parent().append('<a href="http://' + flota + '.framelogic.pl/api/fuel/main/calibration/' + id + '" target="_blank"><img src="/api/media/images/newLayout/info.png"></a>')
+        })
+    }
+    if (url.indexOf("refueling") !== -1) {
+        $(".action_pojazd_id_pojazd_id").each(function () {
+            var id = $(this).attr('value');
+            $(this).parent().append('<a href="http://' + flota + '.framelogic.pl/api/fuel/main/calibration/' + id + '" target="_blank"><img src="/api/media/images/newLayout/info.png"></a>')
+        })
+    }
+})();
