08b8419e0eb8ee61399912435ce2ff8ad64e328a
Amadeusz Opac <amadeuszopac@gmail.com>
2016-08-09 10:06:17 +0200

    LooseQuickstart update, second button

--- Framelogic/LooseQuickstart.js
+++ Framelogic/LooseQuickstart.js
@@ -6,14 +6,17 @@ javascript:(function () {
     if (url.indexOf("loose") !== -1) {
         $(".action_raports_showcard_showvehicle_pojazd_id").each(function () {
             var id = $(this).attr('value');
-            $(this).parent().append('<a href="http://' + flota + '.framelogic.pl/api/fuel/main/calibration/' + id + '" target="_blank"><img src="/api/media/images/newLayout/info.png"></a>')
+            $(this).parent().append('<a href="http://' + flota + '.framelogic.pl/api/fuel/main/calibration/' + id + '" target="_blank"><img class="pagintipsy" src="/api/media/images/newLayout/arrow_copy.png" height="19" width="19"></a>');
+            $(this).parent().append('<a href="http://' + flota + '.framelogic.pl/api/vehicle/calibrationmulti/index/' + id + '" target="_blank"><img src="/api/media/images/newLayout/info.png" height="19" width="19"></a>');
         })
     }
     if (url.indexOf("refueling") !== -1) {
         $(".action_pojazd_id_pojazd_id").each(function () {
             var id = $(this).attr('value');
-            $(this).parent().append('<a href="http://' + flota + '.framelogic.pl/api/fuel/main/calibration/' + id + '" target="_blank"><img src="/api/media/images/newLayout/info.png"></a>')
+            $(this).parent().append('<a href="http://' + flota + '.framelogic.pl/api/fuel/main/calibration/' + id + '" target="_blank"><img src="/api/media/images/newLayout/arrow_copy.png" height="19" width="19"></a>');
+            $(this).parent().append('<a href="http://' + flota + '.framelogic.pl/api/vehicle/calibrationmulti/index/' + id + '" target="_blank"><img src="/api/media/images/newLayout/info.png" height="19" width="19"></a>');
+
         })
     }
-    console.log("LooseQuickstart v1.0")
+    console.log("LooseQuickstart v1.1")
 })();
