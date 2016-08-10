7fb3e4929e2f07d5758832a2fb46d0b03d463ded
Amadeusz Opac <amadeusz.opac@gmail.com>
2016-08-10 08:13:14 +0200

    jquery add

--- Framelogic/index.js
+++ Framelogic/index.js
@@ -6,3 +6,12 @@ function load_website_into_id(website,id){
     var blockHeight = height-229;
     document.getElementById(id).innerHTML='<object type="text/html" data='+website+' style="height: '+blockHeight+'px; width: 100%; overflow: hidden"></object>';
 }
+
+$(document).ready(function(){
+    $("tr.animate").mouseover(function(){
+        $(this).find("td #headie").animate({'background-color':'red'},'fast')
+    });
+    $("tr.animate").mouseout(function(){
+        $(this).find("td #headie").animate({marginLeft:'0px'},0)
+    });
+});
