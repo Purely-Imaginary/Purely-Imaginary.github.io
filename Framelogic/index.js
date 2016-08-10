0c05c301135160d947426b680576692da566e1db
Amadeusz Opac <amadeusz.opac@gmail.com>
2016-08-10 07:44:41 +0200

    index tinker

--- Framelogic/index.js
+++ Framelogic/index.js
@@ -4,5 +4,5 @@
 function load_website_into_id(website,id){
     var height = screen.height;
     var blockHeight = height-229;
-    document.getElementById(id).innerHTML='<object type="text/html" data='+website+' style="height: '+blockHeight+'px; width: 100%"></object>';
+    document.getElementById(id).innerHTML='<object type="text/html" data='+website+' style="height: '+blockHeight+'px; width: 100%; overflow: hidden"></object>';
 }
