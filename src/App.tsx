ef737913cd4aa0573826ad8f851bea734227aabf
Amadeusz Opac <ao@tksolutions.pl>
2020-08-13 13:50:05 +0200

    typescript initial

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+import Counter from './components/mainContainer';
+
+render(<Counter />, document.getElementById('main'));
+
+
+export default App;
