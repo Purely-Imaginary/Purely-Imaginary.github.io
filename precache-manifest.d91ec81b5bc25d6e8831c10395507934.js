65677166a472ab1fcf46e4a0ef8118ae4f63ac70
Amadeusz Opac <amadeuszopacdc@gmail.com>
2020-10-01 13:58:55 +0200

    Updates

--- precache-manifest.d91ec81b5bc25d6e8831c10395507934.js
+++ precache-manifest.d91ec81b5bc25d6e8831c10395507934.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "611e68fc8368e8ed4b241bcd3638f3f2",
+    "revision": "6ab1899ad7b16d8739629483a19166a3",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.11829350.chunk.css"
   },
   {
-    "revision": "b1359ad87e094bc4fbf9",
+    "revision": "6f30afbb3fea95d38692",
     "url": "/static/css/main.3edfe317.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.08e38884.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b1359ad87e094bc4fbf9",
-    "url": "/static/js/main.62e0099c.chunk.js"
+    "revision": "6f30afbb3fea95d38692",
+    "url": "/static/js/main.3c75db9b.chunk.js"
   },
   {
     "revision": "73635f645c271e35ff15",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.618ab5040ecadc7dc43ac807ec7629ea.js"
+  "/precache-manifest.d91ec81b5bc25d6e8831c10395507934.js"
 );
 
 self.addEventListener('message', (event) => {
