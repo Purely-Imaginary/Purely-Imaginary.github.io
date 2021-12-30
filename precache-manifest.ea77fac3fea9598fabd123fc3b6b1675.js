45b75b9d81241aa8a55a9f9adb49a13ba8d46984
Amadeusz Opac <theammanify@gmail.com>
2021-12-30 08:50:20 +0100

    updates

---
+++ precache-manifest.ea77fac3fea9598fabd123fc3b6b1675.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b50d5ab2f16fd54ebfb00f356cf94a64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00de95b9efcb639d89f7",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5284dfabf89dd39ca460",
+    "url": "/static/css/main.2f16b61d.chunk.css"
+  },
+  {
+    "revision": "00de95b9efcb639d89f7",
+    "url": "/static/js/2.37245d03.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/static/js/2.37245d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5284dfabf89dd39ca460",
+    "url": "/static/js/main.89862abc.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "977def0968794abde4dc0395432f3533",
+    "url": "/static/media/littleLoader.977def09.gif"
+  },
+  {
+    "revision": "1bd77fc6b098e6a3bed4776d842e55c8",
+    "url": "/static/media/loader.1bd77fc6.gif"
+  },
+  {
+    "revision": "264d7128ca6f09598028b4ec93c9755b",
+    "url": "/static/media/upload.264d7128.png"
+  }
+]);
